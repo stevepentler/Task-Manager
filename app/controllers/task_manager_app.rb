14db7ef0b43caca21689691ac162906d3841fb0b
Steve Pentler <stevepentler@gmail.com>
2015-12-01 13:40:02 -0700

    not_found error and CRUD comments in task_manager

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -7,22 +7,31 @@ class TaskManagerApp < Sinatra::Base
     erb :dashboard
   end 
 
+  #read
   get '/tasks' do
     @tasks = TaskManager.all
     erb :index
   end
 
+  #create (first half)
   get '/tasks/new' do
     erb :new
   end 
 
+  #create (second half)
   post '/tasks' do
     TaskManager.create(params[:task])
     redirect '/tasks'
   end
 
+  #read
   get '/tasks/:id' do |id|
     @task = TaskManager.find(id.to_i)
     erb :show
   end 
+
+  not_found do 
+    erb :error
+  end 
+
 end 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,11 @@
 class Task
-  attr_reader :title, :description, :id
+  attr_reader :title, :description, :id, :date
 
   def initialize(data)
     @id = data["id"]
     @description = data["description"]
     @title = data["title"]
+    @date= data["date"]
   end
 
   
