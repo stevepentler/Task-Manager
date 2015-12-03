21edf5cef94cf753be6d205f4bdd05dddd64bd69
Steve Pentler <stevepentler@gmail.com>
2015-12-02 20:21:22 -0700

    edit task feature test functional

--- test/features/user_edits_task_and_redirected_to_task_id.rb
+++ test/features/user_edits_task_and_redirected_to_task_id.rb
@@ -1,26 +1,28 @@
 require_relative '../test_helper'
 
 class UserEditsTask < FeatureTest
+    
   def test_task_is_edited
+    TaskManager.create({:title       => "a title",
+                        :description => "a description"})
     #When I visit the task index
-    TaskManager.create({:title => "1 title",
-                        :description => "1 description"})
     visit '/tasks'
     #And I see the tasks
     #And I click the edit link
     click_link("edit")
     #And I get redirected to the edit screen
     #And I enter title
+    assert_equal '/tasks/1/edit', current_path
     within("#edit_form") do 
       fill_in('task[title]', :with => "modified title")
     #And I enter description
       fill_in('task[description]', :with => "modified description")
     #And I click submit 
       click_button("edit_button")
-      save_and_open_page
-      assert page.has_content?("modified title")
-    end 
     #I am redirected to task id
     #Then the title and description have been modified
+      assert page.has_content?("modified title")
+      assert page.has_content?("modified description")
+    end 
   end
 end 
